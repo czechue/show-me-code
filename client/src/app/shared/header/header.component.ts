cb71985212f5b0898a807ac2d4b88108b83a1539
Michal Lester <michal.lester@allegro.pl>
2018-12-01 21:22:50 +0100

    styled navbar links

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -5,49 +5,29 @@ import { Component, OnInit } from '@angular/core';
     template: `
         <header class="header">
             <nav class="navbar">
-                <div class="container wrapper">
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarTogglerDemo03"
-                        aria-controls="navbarToggler"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <a class="navbar-brand header__title" href="#">
-                        Pokaż mi Kod
-                    </a>
-                    <div
-                        class="collapse navbar-collapse text-center"
-                        id="navbarTogglerDemo03"
-                    >
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li class="nav-item active">
-                                <a class="nav-link" routerLink="projects">
-                                    Projects
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Jak zacząć</a>
-                            </li>
-                        </ul>
+                <div class="container wrapper navbar-container">
+                    <a class="navbar-logo" href="#"> Pokaż mi Kod </a>
 
-                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="/auth/google">
-                                    Zaloguj się z Google
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/api/logout">
-                                    Logout
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    <ul class="navbar-nav">
+                        <li class="navbar-item active">
+                            <a class="navbar-link" routerLink="projects">
+                                Projekty
+                            </a>
+                        </li>
+                        <li class="navbar-item">
+                            <a class="navbar-link" href="#">FAQ</a>
+                        </li>
+                        <li class="navbar-item">
+                            <a class="navbar-link" href="/auth/google">
+                                Login
+                            </a>
+                        </li>
+                        <li class="navbar-item">
+                            <a class="navbar-link" href="/api/logout">
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
